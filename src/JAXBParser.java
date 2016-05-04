4e88444dccfed00f3138c8bd0f15a3e0c1b97230
dkomp <dimitris.kompolias@eurodyn.com>
2016-05-04 14:17:22 +0300

    Added functionality to parse an Xml documenrt via JaxB.

---
+++ src/JAXBParser.java
@@ -0,0 +1,32 @@
+import java.io.File;
+
+import generatedCode.*;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ * Created by dkomp on 28/4/2016.
+ */
+public class JAXBParser {
+
+
+    public static void main(String[] args) {
+
+        try {
+            File file = new File("C:\\test.xml");
+            JAXBContext jaxbContext = JAXBContext.newInstance(Employee.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            Employee emp = (Employee)unmarshaller.unmarshal(file);
+            Person per = new Person();
+            per.setAddress("Maikina 13");
+            per.setAge("28");
+            per.setName("Dkompo");
+            System.out.println(per);
+            System.out.println(emp);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
