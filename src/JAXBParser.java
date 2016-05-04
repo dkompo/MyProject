b210f374b7f96f4c27a3e7a2da03b96c2b50bce0
dkomp <dimitris.kompolias@eurodyn.com>
2016-05-04 15:29:59 +0300

    Added a sec feature.

--- src/JAXBParser.java
+++ src/JAXBParser.java
@@ -25,6 +25,7 @@ public class JAXBParser {
             System.out.println(per);
             System.out.println(emp);
             System.out.println("Added a new feature");
+            System.out.println("Added a sec feature");
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
