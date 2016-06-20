e50d91333ad81d9c1b1444e120724e3998b3e9c6
dkomp <dimitris.kompolias@eurodyn.com>
2016-06-20 16:52:02 +0300

    Added class to support Rest client.

---
+++ src/main/java/RestClient.java
@@ -0,0 +1,30 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by dkomp on 20/6/2016.
+ */
+public class RestClient {
+
+     private final static String APIKEY = "b0289f960dd90a58cab00b69c168c922";
+
+    public static void main(String[] args) {
+
+//
+//        try {
+//
+//            URL url = new URL("api.openweathermap.org/data/2.5/weather?q=London&APPID="+APIKEY);
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//            conn.setRequestMethod("GET");
+//            conn.setRequestProperty("Accept", "application/json");
+//
+//            if (conn.getResponseCode() != 200) {
+//                throw new RuntimeException("Failed : HTTP error code : "
+//                        + conn.getResponseCode());
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+}
