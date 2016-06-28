ca929dfa33987834d0bdce54f37079603992c470
dkomp <dimitris.kompolias@eurodyn.com>
2016-06-28 11:53:05 +0300

    Added JAXB parser functionality files.

---
+++ src/main/java/Person.java
@@ -0,0 +1,42 @@
+/**
+ * Created by dkomp on 28/4/2016.
+ */
+public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    private String name;
+    private String address;
+    private String age;
+
+}
